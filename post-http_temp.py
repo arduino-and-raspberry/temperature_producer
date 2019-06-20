cff712c63b767041828978e78ea52d9b6567a69f
Anton Romanov <antonromanov@list.ru>
2019-06-20 17:59:28 +0300

    added serail read

--- post-http_temp.py
+++ post-http_temp.py
@@ -12,19 +12,23 @@ ADD_LOG_URL = "http://84.47.161.121:8080/rest/users/addlog"
 
 ser = serial.Serial("/dev/ttyUSB0", 9600)
 serialLine = ser.readline()
-usertemp = int(serialLine.strip('\0'))
+decodedLine = serialLine.decode('utf-8', 'backslashreplace')
 
-payload = {'temp': usertemp}
-logPayload = {'who': None, 'lastTemperature': -5, 'lastHumidity': 48, 'serverTime': None, 'lastContactTime': None, 'current': 151, 'amperage': 155, 'power': 180, 'consuming': 1024, 'lastContactDate': None, 'acOn': True, 'lanOn': True}
-json_data = json.dumps(payload)
-json_data2 = json.dumps(logPayload)
+if "DHT" not in decodedLine:
+
+    usertemp = int(serialLine.strip('\0'))
+
+    payload = {'temp': usertemp}
+    logPayload = {'who': None, 'lastTemperature': -5, 'lastHumidity': 48, 'serverTime': None, 'lastContactTime': None, 'current': 151, 'amperage': 155, 'power': 180, 'consuming': 1024, 'lastContactDate': None, 'acOn': True, 'lanOn': True}
+    json_data = json.dumps(payload)
+    json_data2 = json.dumps(logPayload)
 
 
-while 1:
-    r = requests.post(ADD_TEMP_URL, data=json_data)
-    print(r.text)
-    time.sleep(10)
+    while 1:
+        r = requests.post(ADD_TEMP_URL, data=json_data)
+        print(r.text)
+        time.sleep(10)
 
-    r = requests.post(ADD_LOG_URL, data=json_data2)
-    print(r.text)
-    time.sleep(TIME_OUT)
+        r = requests.post(ADD_LOG_URL, data=json_data2)
+        print(r.text)
+        time.sleep(TIME_OUT)

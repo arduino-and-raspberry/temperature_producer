f028f51187111e8f62559279e612349e092df778
Anton Romanov <antonromanov@list.ru>
2019-06-20 18:03:10 +0300

    added serail read

--- post-http_temp.py
+++ post-http_temp.py
@@ -12,15 +12,18 @@ ADD_LOG_URL = "http://84.47.161.121:8080/rest/users/addlog"
 
 ser = serial.Serial("/dev/ttyUSB0", 9600)
 serialLine = ser.readline()
-decodedLine = serialLine.decode('utf-8', 'backslashreplace')
+
 
 logPayload = {'who': None, 'lastTemperature': -5, 'lastHumidity': 48, 'serverTime': None, 'lastContactTime': None, 'current': 151, 'amperage': 155, 'power': 180, 'consuming': 1024, 'lastContactDate': None, 'acOn': True, 'lanOn': True}
 json_data2 = json.dumps(logPayload)
 
 while 1:
+    print(serialLine)
+    decodedLine = serialLine.decode('utf-8', 'backslashreplace')
     if "DHT" not in decodedLine:
 
         usertemp = int(serialLine.strip('\0'))
+        print(usertemp)
         payload = {'temp': usertemp}
         json_data = json.dumps(payload)
         r = requests.post(ADD_TEMP_URL, data=json_data)
